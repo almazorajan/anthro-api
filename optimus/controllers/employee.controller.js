c868b7a103f2f923276792f0c41a43ebc12835b0
Jan Almazora <jalmazora@ewise.com>
2017-02-25 01:28:53 +0800

    removed redundancy

--- optimus/controllers/employee.controller.js
+++ optimus/controllers/employee.controller.js
@@ -7,6 +7,26 @@ const EmployeeModel = require("../models/employee/employee.model");
 
 router.use(require("../middlewares/session-validator.middleware").ValidateSession);
 
+router.post("/getall", (req, res) => {
+    try {
+        EmployeeModel.GetAll().then((result) => {
+            res.send(result);
+        })
+        .catch((error) => {
+            res.send(new Result({
+                success: false,
+                message: error.toString()
+            }));
+        });
+    } catch(e) {
+        res.send(new Result({
+            success: false,
+            message: (e || e.message).toString()
+        }));
+    }
+});
+
+
 router.post("/add", (req, res) => {
     try {
         let result = new Result();
@@ -28,6 +48,63 @@ router.post("/add", (req, res) => {
             res.send(new Result({
                 success: false,
                 message: error.toString()
+            }));
+        });
+    } catch(e) {
+        res.send(new Result({
+            success: false,
+            message: (e || e.message).toString()
+        }));
+    }
+});
+
+router.post("/update", (req, res) => {
+    try {
+        let result = new Result();
+        let employee = req.body.data;
+        
+        EmployeeModel.FindOneByEmployeeNumber(employee).then((_result) => {
+            if(_result.success) {
+                EmployeeModel.UpdateById(employee).then((_result) => {
+                    res.send(_result);
+                })
+                .catch((e) => {
+                    res.send(new Result({
+                        success: false,
+                        message: (e || e.message).toString()
+                    }));
+                });
+            } else {
+                result.success = false;
+                result.message = "Employee number does not exist.";
+                res.send(result);
+            }
+        })
+        .catch((e) => {
+            res.send(new Result({
+                success: false,
+                message: (e || e.message).toString()
+            }));
+        });
+    } catch(e) {
+        res.send(new Result({
+            success: false,
+            message: (e || e.message).toString()
+        }));
+    }
+});
+
+router.post("/delete", (req, res) => {
+    try {
+        let employee = req.body.data;
+
+        EmployeeModel.DeleteById(employee).then((result) => {
+            res.send(result);
+        })
+        .catch((e) => {
+            res.send(new Result({
+                success: false,
+                message: (e || e.message).toString()
             }));
         });
     } catch(e) {
