8c8dba6c7b03a70dddd707f93c9402c53386bb72
Jan Almazora <jalmazora@ewise.com>
2017-03-08 00:24:06 +0800

    committing connection

--- optimus/optimus.con.js
+++ optimus/optimus.con.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const development = true;
+const productionConnection = "mongodb://admin1:admin1@ds049436.mlab.com:49436/heroku_rcxwb9tv";
+const developmentConnection = "mongodb://admin1:admin1@ds121190.mlab.com:21190/heroku_sk7s21zx";
 
 mongoose.Promise = require("bluebird");
-mongoose.connect("mongodb://admin1:admin1@ds049436.mlab.com:49436/heroku_rcxwb9tv");
+mongoose.connect(development ? developmentConnection : productionConnection);
 
 module.exports = mongoose;
