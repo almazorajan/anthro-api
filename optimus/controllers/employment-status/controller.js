69f9163375f0da70716e45ae504078f81985105d
Jan Almazora <jalmazora@ewise.com>
2017-03-30 23:07:58 +0800

    fixes

--- optimus/controllers/employment-status/controller.js
+++ optimus/controllers/employment-status/controller.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(require("../../middlewares/session-validator.middleware").ValidateSession);
 
 require("./endpoints/get-all.endpoint")(router);
-require("./endpoints/get-all.endpoint")(router);
+require("./endpoints/add.endpoint")(router);
 require("./endpoints/update.endpoint")(router);
 require("./endpoints/delete.endpoint")(router);
 
