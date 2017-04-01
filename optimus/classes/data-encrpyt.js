676a80a1b16d5eb8d2fe33e403e5899af09307d1
Jan Almazora <jalmazora@ewise.com>
2017-04-01 12:13:01 +0800

    committing changes

--- optimus/classes/data-encrpyt.js
+++ optimus/classes/data-encrpyt.js
@@ -1,12 +1,12 @@
-"use strict";
+// "use strict";
 
-const FieldEncryption = require("mongoose-field-encryption").fieldEncryption;
-const process = require("process");
-const SECRET = process.env.SECRET ? process.env.SECRET : "fgkie24!2@$%@$@^GhGqQwWGf";
+// const FieldEncryption = require("mongoose-field-encryption").fieldEncryption;
+// const process = require("process");
+// const SECRET = process.env.SECRET ? process.env.SECRET : "fgkie24!2@$%@$@^GhGqQwWGf";
 
-module.exports = (Schema, fields) => {
-    Schema.plugin(FieldEncryption, {
-        fields: fields,
-        secret: SECRET
-    })
-};
+// module.exports = (Schema, fields) => {
+//     Schema.plugin(FieldEncryption, {
+//         fields: fields,
+//         secret: SECRET
+//     })
+// };
