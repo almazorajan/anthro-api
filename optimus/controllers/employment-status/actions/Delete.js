3ca88754a62bbb1cb8c4b572efe64b8684ee09ba
Jan Almazora <janalmazora@Jans-MacBook-Pro.local>
2017-08-29 14:09:05 +0800

    Comm

--- optimus/controllers/employment-status/actions/Delete.js
+++ optimus/controllers/employment-status/actions/Delete.js
@@ -9,11 +9,11 @@ module.exports = (req, res) => {
     try {
         let employmentStatus = req.body.data;
 
-        let check1 = Employee.CountByEmploymentStatusId(employmentStatus._id);
-        let check2 = Employee.CountByWorkHistoryEmploymentStatusId(employmentStatus._id);
-
         Promise
-            .all([check1, check2])
+            .all([
+                Employee.CountByEmploymentStatusId(employmentStatus._id),
+                Employee.CountByWorkHistoryEmploymentStatusId(employmentStatus._id)
+            ])
             .then((res) => {
                 if (res[0] <= 0 && res[1] <= 0)
                     return new EmploymentStatus(employmentStatus).DeleteById();
